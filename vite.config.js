c49e9dd5d418a7f15b5243f8aad3d9f0d32dcb89
cloudsoftpanama <cloudsoftpanama@gmail.com>
2026-07-28 23:57:08 -0500

    Final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // ruta relativa: funciona igual en GitHub Pages, Netlify o abriendo el archivo
-  base: './',
+  base: '/',
   plugins: [react()],
   build: { outDir: 'dist', assetsDir: 'assets' },
 });
